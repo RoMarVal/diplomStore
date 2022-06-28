22d2490f60512c6d9957551ccdcd2b4836932dfb
Lidiya <lima07@mail.ru>
2022-06-29 01:08:04 +0400

    разделила контроллеры

--- src/main/java/com/first/shop2206/controllers/ProductController.java
+++ src/main/java/com/first/shop2206/controllers/ProductController.java
@@ -1,9 +1,6 @@
 package com.first.shop2206.controllers;
 
 import com.first.shop2206.model.Product;
-import com.first.shop2206.model.User;
-import com.first.shop2206.repositories.ProductRepository;
-import com.first.shop2206.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,21 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.first.shop2206.repositories.ProductRepository;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
-public class MainController {
+public class ProductController {
     @Autowired
     private ProductRepository productRepository;
-    private UserRepository userRepository;
 
-    @GetMapping("/")
-    public String mainPage(Model model) {
-        model.addAttribute("title", "Главная страница");
-        return "mainPage";
-    }
     @GetMapping("/admin")
     public String admin(Model model) {
         model.addAttribute("title", "Страница администратора");
@@ -99,73 +91,4 @@ public class MainController {
         return "redirect:/products";
     }
 
-    @GetMapping("/admin/addUser")
-    public String adminAddUser(Model model) {
-        return "addUser";
-    }
-
-    @GetMapping("/allUsers")
-    public String allUsers(Model model) {
-        Iterable<User> allUsers = userRepository.findAll();
-        model.addAttribute("allUsers", allUsers);
-        return "allUsers";
-    }
-
-    @PostMapping("/admin/addUser")
-    public String adminUserAdd(@RequestParam String username, @RequestParam String password, @RequestParam String e_mail, @RequestParam String phoneNumber, Model model) {
-        User user = new User(username,password,e_mail,phoneNumber);
-        userRepository.save(user);
-        return "redirect:/admin/addUser";}
-    @GetMapping("/allUsers/{id}")
-    public String allUsersDetails(@PathVariable(value = "id") long id, Model model) {
-        if (!userRepository.existsById(id)) {
-            return "redirect:/allUsers";
-        }
-        Optional<User> user = userRepository.findById(id);
-        ArrayList<User> res = new ArrayList<>();
-        user.ifPresent(res::add);
-        model.addAttribute("user", res);
-        return "allUsersDetails";
-    }
-    @GetMapping("/allUsers/{id}/edit")
-    public String allUsersEdit(@PathVariable(value = "id") long id, Model model) {
-        if (!userRepository.existsById(id)) {
-            return "redirect:/allUsers";
-        }
-        Optional<User> user = userRepository.findById(id);
-        ArrayList<User> res = new ArrayList<>();
-        user.ifPresent(res::add);
-        model.addAttribute("user", res);
-        return "allUsersEdit";}
-
-    @PostMapping("/allUsers/{id}/edit")
-    public String adminUserUpdate(@PathVariable(value = "id") long id, @RequestParam String username, @RequestParam String password, @RequestParam String e_mail, @RequestParam String phoneNumber, Model model) {
-        User user= new User(username, password,e_mail,phoneNumber);
-        if (!userRepository.existsById(id)) {
-            return "redirect:/allUsers";
-        }
-        User users = userRepository.findById(id).orElseThrow(IllegalStateException::new);
-        users.setUsername(username);
-        users.setPassword(password);
-        users.setE_mail(e_mail);
-        users.setPhoneNumber(phoneNumber);
-        userRepository.save(users);
-        return "redirect:/allUsers";
-    }
-
-    @PostMapping("/allUsers/{id}/remove")
-    public String adminUserDelete(@PathVariable(value = "id") long id, Model model) {
-        User user= userRepository.findById(id).orElseThrow(IllegalStateException::new);
-        userRepository.delete(user);
-        return "redirect:/allUsers";
-    }
-
-    @GetMapping("/basket")
-    public String basket(Model model) {
-        model.addAttribute("title", "Корзина");
-        return "basket";
-    }
-    @GetMapping("/hello")
-    public String securityUrl(){return "helloPage";}
 }
-
